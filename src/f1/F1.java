ddcc719a73d53148b5dc89405dadaed8996518df
asmalouski <asmalouski@asmalouski-ubuntu>
2014-10-12 12:54:11 +0300

    first commit

---
+++ src/f1/F1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package f1;
+
+
+import javax.swing.*;
+/**
+ *
+ * @author asmalouski
+ */
+public class F1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame j = new JFrame("Java F1");
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setSize(1100, 600);
+        j.setVisible(true);
+    }
+    
+}
